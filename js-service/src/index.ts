ba92b58bcc0b3de6646c020d64e899b95a15b19f
Dhiren-codity <dhiren-codity@example.com>
2026-07-04 01:01:51 +0530

    Add rereview one-line test changes

--- js-service/src/index.ts
+++ js-service/src/index.ts
@@ -106,7 +106,7 @@ const app: Express = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
